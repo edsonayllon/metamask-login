d4249cc6cdf0856a356f006b8695d2c12b9558d1
Edson Ayllon <EAYLLON1@GMAIL.COM>
2019-07-09 20:59:50 +0000

    removed argon

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const argon2 = require('argon2');
 
 // If user does not validate their email within 24 hrs,
 // their account will be deleted by default
@@ -7,12 +6,5 @@ const UserSchema = new mongoose.Schema({
   address: { type: String, required: false, unique: true },
   nonce: { type: int, required: false, default: () => Math.floor(Math.random() * 1000000),  }
 });
-
-// Argon2 is used to hash passwords instead of bcrypt https://password-hashing.net/
-UserSchema.methods.isValidPassword = async function(password){
-  const user = this;
-  const verify = await argon2.verify(user.local.password, password);
-  return verify;
-}
 
 module.exports = mongoose.model('user', UserSchema);
